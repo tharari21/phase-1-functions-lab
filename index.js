57c449bbe97139bf0939aa7123f646caae63c64d
Tomer Harari <tharari21@gmail.com>
2022-07-05 09:55:14 -0400

    commit tree 18451016ec14fc098e1338f90faa9e7e290af274

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+  return Math.abs(pickupLocation - 42);
+}
+function distanceFromHqInFeet(pickupLocation) {
+  let numberOfBlocks = distanceFromHqInBlocks(pickupLocation);
+  return 264 * numberOfBlocks;
+}
+function distanceTravelledInFeet(startBlock, destinationBlock) {
+  return Math.abs((destinationBlock - startBlock) * 264);
+}
+function calculatesFarePrice(startBlock, endBlock) {
+  let distance = distanceTravelledInFeet(startBlock, endBlock);
+  if (distance <= 400) {
+    return 0;
+  } else if (distance <= 2000) {
+    return 0.02 * (distance - 400);
+  } else if (distance <= 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
